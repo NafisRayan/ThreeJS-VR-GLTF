bc5bb5d1a5b55c66e62aefcdf36606e24118356a
Nafis Rayan <nafisrayan123@gmail.com>
2024-09-26 04:02:59 +0600

    update marged

--- js/main.js
+++ js/main.js
@@ -10,8 +10,9 @@ const scene = new THREE.Scene();
 
 // Set up a Perspective Camera with field of view, aspect ratio, and clipping planes
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 10; // Set the camera's z position to see the models
-
+camera.position.z = 12; // Set the camera's z position to see the models
+camera.position.y = 3;
+camera.position.x = 0;
 // Create a WebGL renderer with transparency enabled
 const renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight); // Set the renderer size
@@ -23,18 +24,20 @@ const controls = new OrbitControls(camera, renderer.domElement);
 // GLTF model folders
 const modelsFolders = [
   { folder: 'mars_surface_terrain_model', scale: [50, 50, 50], position: [270,85,-500], rotation: [-.04, Math.PI, 0], animation: false },
-  { folder: 'curiosity_rover', scale: [1, 1, 1], position: [5, -3, -5], rotation: [0, Math.PI, 0], animation: true },
-  { folder: 'astronaut', scale: [2, 2, 2], position: [-5, -4.5, -3], rotation: [0, 0, 0], animation: true },
-  { folder: 'perseverance_mars_rover', scale: [1, 1, 1], position: [-11, -3, -5], rotation: [0, Math.PI, 0], animation: false },
+  { folder: 'curiosity_rover', scale: [2.5, 2.5, 2.5], position: [-2, -4, -25], rotation: [0, 0, 0], animation: true },
+  { folder: 'astronaut', scale: [2, 2, 2], position: [-12, -4.5, 2], rotation: [0, -250.8, 0], animation: true },
+  { folder: 'perseverance_mars_rover', scale: [2, 2, 2], position: [-18, -3, -12], rotation: [0, Math.PI, 0], animation: false },
   // { folder: 'mariner_4_spacecraft', scale: [1, 1, 1], position: [8, 18, -40], rotation: [0, Math.PI, 0], animation: false },
-  { folder: 'space_shuttle', scale: [.5,.5,.5], position: [-40,-1, -40], rotation: [0, -90, 0], animation: false },
-  { folder: 'robot_from_the_series_love_death_and_robots', scale: [.2, .2, .2], position: [8,0.-2 , 3], rotation: [0, 110.5, 0], animation: true },
-  { folder: 'planet_earth', scale: [5, 5, 5], position: [25, 18, -100], rotation: [0, Math.PI, 0], animation: false },
+  { folder: 'space_shuttle', scale: [.9,.9,.9], position: [-50,-1, -120], rotation: [0, -200, 0], animation: false },
+  { folder: 'robot_from_the_series_love_death_and_robots', scale: [.3, .3, .3], position: [8, 0.-2 , 5], rotation: [0, 10.2, 0], animation: false },
+  { folder: 'planet_earth', scale: [20, 20, 20], position: [25, 50, -500], rotation: [0, Math.PI, 0], animation: false },
   { folder: 'planet', scale: [.0023, .0023, .0023], position: [-10, 18, -50], rotation: [0, Math.PI, 0], animation: false },
-  { folder: 'solar_skid', scale: [.01,.01,.01], position: [10, -2.3, -5], rotation: [0, 0, 0], animation: false },
-  { folder: 'solar_skid', scale: [.01,.01,.01], position: [10, -2.3, -10], rotation: [0, 0, 0], animation: false },
-  { folder: 'solar_skid', scale: [.01,.01,.01], position: [10, -2.3, -13], rotation: [0, 0, 0], animation: false },
-  { folder: 'solar_skid', scale: [.01,.01,.01], position: [10, -2.3, -8], rotation: [0, 0, 0], animation: false }
+  { folder: 'planet', scale: [1, 1, 1], position: [-5, 18, -50], rotation: [0, Math.PI, 0], animation: false },
+  // { folder: 'solar_skid', scale: [.03,.03,.04], position: [14.5, -2.3, -3], rotation: [0, -269.3, 0], animation: false },
+  { folder: 'solar_skid', scale: [.03,.03,.04], position: [13, -2.3, -11], rotation: [0, -269.3, 0], animation: false },
+  { folder: 'solar_skid', scale: [.03,.03,.04], position: [12, -2.3, -19], rotation: [0, -269.3, 0], animation: false },
+  { folder: 'solar_skid', scale: [.03,.03,.04], position: [11, -2.3, -28], rotation: [0, -269.3, 0], animation: false },
+  { folder: 'solar_skid', scale: [.03,.03,.04], position: [10, -2.3, -37], rotation: [0, -269.3, 0], animation: false }
 ];
 
 let models = []; // Array to store loaded models
